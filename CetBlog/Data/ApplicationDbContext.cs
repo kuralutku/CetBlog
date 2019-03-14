746730495e480ed5f12f98ed68088a0c196a394b
kuralutku <kuralutku@gmail.com>
2019-03-14 13:21:33 +0300

    Initial

--- CetBlog/Data/ApplicationDbContext.cs
+++ CetBlog/Data/ApplicationDbContext.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CetBlog.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CetBlog.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext //F12
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }     //!!!
+        public DbSet<Post> Posts { get; set; }              //!!!
+        public DbSet<Comment> Comments{ get; set; }         //!!!
+
     }
 }
---
+++ CetBlog/Data/Migrations/20190314090804_Inital Model.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CetBlog.Data.Migrations
+{
+    public partial class InitalModel : Migration  //"Inıtial" is special, so that we type it purposely incorrect
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
